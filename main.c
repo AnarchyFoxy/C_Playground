d3a7e3e41eff65160bc33e828a08c17a9264ce6a
Astryda Malinowska <astamalinowska@outlook.com>
2022-12-26 17:56:40 +0100

    First example

--- main.c
+++ main.c
@@ -7,6 +7,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    //3.1 Value of your body weight in gold
+    float ciezar, wartosc; //values of weight and in gold
+
+    printf("Czy jesteś wart tyle, co Twój ciężar w złocie?\n");
+    printf("Sprawdźmy\n");
+    printf("Podaj swój ciężar w kilogramach: ");
+
+    //taking values from the user:
+    scanf("%f", &ciezar);
+
+    //I don't know the current gold value, so, I'm taking 4250 for unit
+    wartosc = 4250.0 * ciezar * 32.151;
+
+    printf("Twój cięzar w złocie jst wart %.2f zł.\n", wartosc);
+    printf("Z pewnością jesteś tyle wart! W razie spadku cen złota\n");
+    printf("Jedz więcej, aby utrzymać swoją wartość");
     return 0;
 }
