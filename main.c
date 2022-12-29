1276a806ed5c1b6c48b051e9d20e9f0f360c750a
Astryda Malinowska <astamalinowska@outlook.com>
2022-12-29 16:18:06 +0100

    Started new branch

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 //  main.c
 //  C Playground
 //
-//  Created by Astryda Malinowska (KyriaFoxy) on 22/12/2022.
+//  Created by Astryda Malinowska (KyriaFoxy) on 29/12/2022.
 //
 
 #include <stdio.h>
