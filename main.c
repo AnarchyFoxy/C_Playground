475f4522631ec497887a90ed0a2c4e3001964b64
Astryda Malinowska <astamalinowska@outlook.com>
2023-01-01 20:46:28 +0100

    started new chapter

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 //  main.c
 //  C Playground
 //
-//  Created by Astryda Malinowska (KyriaFoxy) on 22/12/2022.
+//  Created by Astryda Malinowska (KyriaFoxy) on 01/01/2022.
 //
 
 #include <stdio.h>
 
-int main() {
+int main(void) {
     printf("Hello, World!\n");
     return 0;
 }
